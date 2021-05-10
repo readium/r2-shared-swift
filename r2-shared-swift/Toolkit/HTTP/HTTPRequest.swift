0179da1e19e65943d8ecc901d4845c9dd5ac1897
Mickaël Menu <mickael.menu@gmail.com>
2021-05-10 16:59:15 +0200

    Add HTTPRequest.setPostForm

--- r2-shared-swift/Toolkit/HTTP/HTTPRequest.swift
+++ r2-shared-swift/Toolkit/HTTP/HTTPRequest.swift
@@ -13,7 +13,7 @@ public struct HTTPRequest: Equatable {
     public var url: URL
 
     /// HTTP method to use for the request.
-    public var method: Method = .get
+    public var method: Method
 
     /// Supported HTTP methods.
     public enum Method: String, Equatable {
@@ -26,10 +26,10 @@ public struct HTTPRequest: Equatable {
     }
 
     /// Additional HTTP headers to use.
-    public var headers: [String: String] = [:]
+    public var headers: [String: String]
 
     /// The data sent as the message body of a request, such as for an HTTP POST request.
-    public var body: Body? = nil
+    public var body: Body?
 
     /// Supported body values.
     public enum Body: Equatable {
@@ -38,13 +38,31 @@ public struct HTTPRequest: Equatable {
     }
 
     /// The timeout interval of the request.
-    public var timeoutInterval: TimeInterval? = nil
+    public var timeoutInterval: TimeInterval?
 
     /// If true, the user might be presented with interactive dialogs, such as popping up an authentication dialog.
-    public var allowUserInteraction: Bool = false
+    public var allowUserInteraction: Bool
 
     /// Additional context data specific to a given implementation of `HTTPClient`.
-    public var userInfo: [AnyHashable: AnyHashable] = [:]
+    public var userInfo: [AnyHashable: AnyHashable]
+
+    public init(
+        url: URL,
+        method: Method = .get,
+        headers: [String: String] = [:],
+        body: Body? = nil,
+        timeoutInterval: TimeInterval? = nil,
+        allowUserInteraction: Bool = false,
+        userInfo: [AnyHashable: AnyHashable] = [:]
+    ) {
+        self.url = url
+        self.method = method
+        self.headers = headers
+        self.body = body
+        self.timeoutInterval = timeoutInterval
+        self.allowUserInteraction = allowUserInteraction
+        self.userInfo = userInfo
+    }
 
     /// Issue a byte range request. Use -1 to download until the end.
     public mutating func setRange(_ range: Range<UInt64>) {
@@ -60,6 +78,29 @@ public struct HTTPRequest: Equatable {
     public func hasHeader(_ name: String) -> Bool {
         let name = name.lowercased()
         return headers.contains { n, _ in n.lowercased() == name }
+    }
+
+    /// Initializes a POST request with the given form data.
+    public mutating func setPOSTForm(_ form: [String: String?]) {
+        method = .post
+        headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+        body = form
+            .map { key, value in "\(key)=\(encode(value ?? ""))" }
+            .joined(separator: "&")
+            .data(using: .utf8)
+            .map { .data($0) }
+
+        /// https://useyourloaf.com/blog/how-to-percent-encode-a-url-string/#encoding-for-x-www-form-urlencoded
+        func encode(_ s: String) -> String {
+            let unreserved = "*-._ "
+            let allowed = NSMutableCharacterSet.alphanumeric()
+            allowed.addCharacters(in: unreserved)
+
+            return s.addingPercentEncoding(withAllowedCharacters: allowed as CharacterSet)?
+                .replacingOccurrences(of: " ", with: "+")
+                ?? ""
+        }
     }
 }
 
